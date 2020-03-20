11341bec29e9a51ec898b8a691c58214db75c51b
Alex Ratushnyi <alexander.ratushnyi@gmail.com>
2020-03-20 19:39:44 +0200

    🚑 Scroll router

--- src/router/index.js
+++ src/router/index.js
@@ -26,6 +26,13 @@ const routes = [
 
 const router = new VueRouter({
   mode: 'history',
+  scrollBehavior (to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { x: 0, y: 0 }
+    }
+  },
   base: process.env.BASE_URL,
   routes
 })
